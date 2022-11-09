9df7e05347c999c44cfab485f08c5d8cee412312
Paul McGinley <paul.mcginley@eso.com>
2022-11-09 15:01:48 +0000

    piped and mapped api object to my user object. Display correctly.

--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core'; //This import is needed to inject the http request service which will allow us to get the Api and to be used in the
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { User } from '../interface/user.interface';
+import { Response } from '../interface/response.interface';
 //user details modules.
 
 @Injectable({
@@ -20,31 +21,37 @@ export class UsersService {
   }
 
   getUserById(id: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/?id=${id}`);
+    return this.http
+      .get<any>(`${this.apiUrl}/?id=${id}`)
+      .pipe(map((response) => this.processResponseUser(response)));
   }
 
   // getUsers(size: number = 10): Observable<any> {
   //   return this.http
   //     .get<any>(`${this.apiURL}/?results=${size}`)
   //     .pipe(map((response) => this.processResponse(response)));
-}
 
-// private processResponse(response: Response): Response {
-//     return {
-//       info: { ...response.info },
-//       results: response.results.map(
-//         (user: any) => <User>(<unknown>{
-//             id: user.login.uuid,
-//             firstName: user.name.first,
-//             lastName: user.name.last,
-//             email: user.email,
-//             userName: user.login.username,
-//             gender: user.gender,
-//             address: `${user.location.street.number} ${user.location.street.name} ${user.location.city}, ${user.location.country}`,
-//             dateOfBirth: user.dob.date,
-//             phone: user.phone,
-//             imageUrl: user.picture.medium,
-//             coordinate: {
-//               latitude: +user.location.coordinates.latitude,
-//               longitude: +user.location.coordinates.longitude,
-//             },
+  private processResponseUser(response: Response): Response {
+    return {
+      info: { ...response.info },
+      results: response.results.map(
+        (user: any) => <User>(<unknown>{
+            id: user.login.uuid,
+            firstName: user.name.first,
+            lastName: user.name.last,
+            email: user.email,
+            userName: user.login.username,
+            gender: user.gender,
+            address: `${user.location.street.number} ${user.location.street.name} ${user.location.city}, ${user.location.country}`,
+            dateOfBirth: user.dob.date,
+            phone: user.phone,
+            imageUrl: user.picture.medium,
+            coordinate: {
+              latitude: +user.location.coordinates.latitude,
+              longitude: +user.location.coordinates.longitude,
+            },
+          })
+      ),
+    };
+  }
+}
