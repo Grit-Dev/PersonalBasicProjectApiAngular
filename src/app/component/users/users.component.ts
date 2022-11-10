65c16bee06b52d0690208f4b05ff9d86688906a2
Paul McGinley <paul.mcginley@eso.com>
2022-11-10 15:35:59 +0000

    Create functionality for my own onject of an Api working

--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Info } from 'src/app/interface/info.interface';
 import { User } from 'src/app/interface/user.interface';
+import { UserTypicode } from 'src/app/interface/userTypicode.interface';
 import { UsersService } from 'src/app/service/users.service';
 
 @Component({
@@ -9,12 +10,43 @@ import { UsersService } from 'src/app/service/users.service';
   styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
-  
   //felds:
   response: Response | any;
   info: Info | any;
   userById: User | any;
-  responseID: Response| any;
+  responseID: Response | any;
+
+  //
+  //My post object:
+  //
+
+  private userPostApiObj: UserTypicode = {
+    id: 100,
+    name: 'Paul Blackthorne',
+    username: 'hatamoto',
+    email: 'paulsan@testeremail.com',
+    address: {
+      street: 'Toranaga',
+      suite: 'Yeamon',
+      city: 'Osaka',
+      zipcode: '32432432wefdwefew',
+      geo: {
+        lat: '324423423434324',
+        lng: '4645646456456456',
+      },
+    },
+    phone: '345345435345345',
+    website: 'www.shogun.com',
+    company: {
+      name: 'Taranaga-sama-minawara',
+      catchPhrase: 'marikosan',
+      bs: '2000 ki',
+    },
+  };
+
+  //
+  // My Post Object
+  //
 
   //Constructor:
   constructor(private userService: UsersService) {}
@@ -22,10 +54,20 @@ export class UsersComponent implements OnInit {
   ngOnInit(): void {
     this.getUsers();
     this.getUserById();
+
+    //Working! 
+    this.onCreateUser();
   }
 
-  createUser(){
-    
+  onCreateUser(): void {
+    this.userService.CreateUser(this.userPostApiObj).subscribe({
+      next: (result: any) => {
+        console.table(this.response);
+      },
+      error: () => {
+        console.log('Post request did not work');
+      },
+    });
   }
 
   getUserById() {
@@ -33,7 +75,6 @@ export class UsersComponent implements OnInit {
       .getUserById('e61141a0-4ff1-44d5-8093-e903b61d9aca')
       .subscribe({
         next: (results: any) => {
-          
           this.responseID = results as unknown as User;
 
           //Trying my on logic to check if I fully understand this - Merely testing
@@ -49,18 +90,16 @@ export class UsersComponent implements OnInit {
   getUsers() {
     this.userService.getUsers(10).subscribe({
       next: (results: any) => {
-
         this.response = results as unknown as Response;
-        this.info = {...results.info} as unknown as Info;
+        this.info = { ...results.info } as unknown as Info;
 
         //Trying my on logic to check if I fully understand this - Merely testing
-        
-        //Might only need to do this.responsClass = response;
 
+        //Might only need to do this.responsClass = response;
       },
       error: () => {
-      console.log('Response did not happen');
-      }
+        console.log('Response did not happen');
+      },
     });
   }
 }
